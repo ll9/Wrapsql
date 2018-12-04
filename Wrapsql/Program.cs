0b5f404ef67a032b19b1cd11f3fd666e6391d263
ll9 <llwierer@web.de>
2018-12-04 20:14:29 +0100

    Projektdateien hinzufügen.

---
+++ Wrapsql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrapsql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
